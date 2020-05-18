a524023a9080194f5e5cb64ef84ea37d09bf1bbd
Levon <5novel5@Mail.ru>
2020-05-18 10:32:20 +0300

    Поиск цепи Эйлера

---
+++ Graf_Eiler/Graf_Eiler/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Graf_Eiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(17, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(197, 176);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите матрицу смежности";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(17, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Проверить граф";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(500, 300);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(492, 274);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Полуэйлеров";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(492, 274);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Брон - Кербош";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(275, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "   ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Graf_Eiler/Graf_Eiler/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graf_Eiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Graf
+        {
+            public struct Vertex
+            {
+                string date;
+
+                public Vertex(string new_date)
+                {
+                    this.date = new_date;
+                }
+                public void setVertexData(string new_date)
+                {
+                    this.date = new_date;
+                }
+                public string getVertexData()
+                {
+                    return this.date;
+                }
+            }
+            public struct Edge
+            {
+                Vertex stockVertex;
+                Vertex sourceVertex;
+                string inf;
+                public Edge(Vertex new_stockVertex, Vertex new_sourceVertex)
+                {
+                    this.stockVertex = new_stockVertex;
+                    this.sourceVertex = new_sourceVertex;
+                    this.inf = null;
+                }
+                public Edge(Vertex new_stockVertex, Vertex new_sourceVertex, string new_inf)
+                {
+                    this.stockVertex = new_stockVertex;
+                    this.sourceVertex = new_sourceVertex;
+                    this.inf = new_inf;
+                }
+                public void setStockVertex(Vertex new_stockVertex)
+                {
+                    this.stockVertex = new_stockVertex;
+                }
+                public Vertex getStockVertex()
+                {
+                    return this.stockVertex;
+                }
+                public void setSourceVertex(Vertex new_sourceVertex)
+                {
+                    this.sourceVertex = new_sourceVertex;
+                }
+                public Vertex getSourceVertex()
+                {
+                    return this.sourceVertex;
+                }
+                public void setInf(string new_inf)
+                {
+                    this.inf = new_inf;
+                }
+                public string getInf()
+                {
+                    return this.inf;
+                }
+            }
+            List<Vertex> list_vertex;
+            List<Edge> list_edge;
+
+            public void add_to_edge_list(Edge edge_to_add)
+            {
+                list_edge.Add(edge_to_add);
+            }
+            public void del_from_edge_list(string inf_to_del)
+            {
+                foreach (Edge e in list_edge)
+                {
+                    if (e.getInf() == inf_to_del)
+                    {
+                        list_edge.Remove(e);
+                    }
+                }
+            }
+            /*
+            public Edge find_from_edge_list(string inf_to_find)
+            {
+                foreach (Edge e in list_edge)
+                {
+                    if (e.getInf() == inf_to_find)
+                    {
+                        return e;
+                    }
+                }
+            }
+            */
+            public void add_to_vertex_list(Vertex vertex_to_add)
+            {
+                list_vertex.Add(vertex_to_add);
+            }
+            public void del_from_vertex_list(string date_to_del)
+            {
+                foreach (Vertex v in list_vertex)
+                {
+                    if (v.getVertexData() == date_to_del)
+                    {
+                        list_vertex.Remove(v);
+                    }
+                }
+            }
+            /*
+            public Vertex find_from_vertex_list(string date_to_find)
+            {
+                foreach (Vertex v in list_vertex)
+                {
+                    if (v.getVertexData() == date_to_find)
+                    {
+                        return v;
+                    }
+                }
+            }
+            */
+            //Описать для стока и истока
+            /*
+             
+             
+             
+             
+             
+             */
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string matSt = richTextBox1.Text;
+            String[] str = matSt.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);//Разбиваем матрицу на строки
+            int n = str.Length;
+            int z = 0;
+            String[,] mass = new string[n, n];
+            String[] pointsFF = matSt.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);//Разбиваем матрицу на символы
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    mass[i, j] = pointsFF[z];
+                    z++;
+                }
+            }
+            int[] prov_mass = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mass[i, j] != "0")
+                        prov_mass[i]++;
+                }
+            }
+            int chetchik = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (prov_mass[i]%2==1)
+                    chetchik++;
+            }
+            if (chetchik==2)
+                label2.Text = "Граф является\nполуэйлеровым!";
+            else
+                label2.Text = "Граф НЕ является\nполуэйлеровым!";
+            //MessageBox.Show("Граф является полуэйлеровым!");
+        }
+
+    }
+}
